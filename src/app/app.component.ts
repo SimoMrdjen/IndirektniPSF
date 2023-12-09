ddd157c1dbdd6498e8a96be2f158fc853c2fa29d
Simo <dr.sizni@gmail.com>
2023-12-09 22:20:51 +0100

    for sure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Role } from './models/role.model';
 import { ObrazacService } from './services/obrazac.service';
 import { KvartalService } from './services/kvartal.service';
 import { Subscription } from 'rxjs';
+import { IndirektniService } from './services/indirektni.service';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +18,26 @@ export class AppComponent implements OnInit {
   kvartal: number | undefined;
   indirektni?: string;
   private kvartalSubscription?: Subscription;
+  private indirektniSubscription?: Subscription;
 
-  constructor(private router: Router, private kvartalService: KvartalService) {}
+  constructor(
+    private router: Router,
+    private kvartalService: KvartalService,
+    private indirektniService: IndirektniService
+  ) {}
   ngOnInit(): void {
     const roleStr = localStorage.getItem('role');
     if (roleStr) {
       this.role = Role[roleStr as keyof typeof Role];
     }
     this.indirektni = localStorage.getItem('indirektni') || undefined;
+
+    // this.indirektniSubscription = this.indirektniService.indirektni$.subscribe(
+    //   (indirektni) => {
+    //     this.indirektni = localStorage.getItem('indirektni') || undefined;
+    //     console.log('This is kvartal from app onInit : ' + indirektni);
+    //   }
+    // );
 
     this.kvartalSubscription = this.kvartalService.kvartal$.subscribe(
       (kvartal) => {
