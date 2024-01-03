56f604a51b8396169944a8b98c9295715f6ac351
Simo Mrdjen <dr.sizni@gmail.com>
2024-01-03 09:47:20 +0100

    bogdan refactor+disabled io and obr5

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 // app-constants.ts
-//export const BASE_URL = 'http://localhost:8080/api/';
+export const BASE_URL = 'http://localhost:8080/api/';
 
-export const BASE_URL = 'http://185.166.125.138:8080/api/';
+//export const BASE_URL = 'http://185.166.125.138:8080/api/';
  
