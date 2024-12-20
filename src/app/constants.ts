1eb1b6a95da7bfb8b9d3612875d3d62a7e7ece2f
Simo Mrdjen <dr.sizni@gmail.com>
2024-12-20 14:04:05 +0100

    obr5 final

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,6 @@
 // app-constants.ts
-//export const BASE_URL = 'http://localhost:8080/api/';
+export const BASE_URL = 'http://localhost:8080/api/';
 
-export const BASE_URL = 'http://185.166.125.138:8080/api/';
+//export const BASE_URL = 'http://185.166.125.138:8080/api/';
  
+// export const BASE_URL = 'http://192.168.5.107:8080/api/';
